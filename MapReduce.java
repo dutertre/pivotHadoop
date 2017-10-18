968da59fbc90f95504fbd0bc15ceba43558404b2
Sébastien DUTERTRE <dutertre.seb@free.fr>
2017-10-18 22:46:59 +0200

    Ajout des commentaires

--- MapReduce.java
+++ MapReduce.java
@@ -17,7 +17,9 @@ public class MapReduce {
 
 		 		long col = 0;
 		 		long line = key.get();
+		 		//Découpage des lignes avec le délimiteur par défaut des CSV, le point-virgule
 		 		for (String num : value.toString().split(";")) {
+		 			//Créer une colonne pour chaque valeur récupérée
 		 			context.write(new LongWritable(col), new Text(line + "\t" + num));
 		 			++col;
 		 		}		 			
@@ -27,17 +29,20 @@ public class MapReduce {
 	 public static class PivotReducer extends Reducer<LongWritable, Text, LongWritable, Text> {
 		 @Override
 		 protected void reduce(LongWritable key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
-			 TreeMap<Long, String> row = new TreeMap<Long, String>();
+			 TreeMap<Long, String> line = new TreeMap<Long, String>();
              for (Text text : values) {
+            	 //Récupération dans une liste les valeurs délimités par un tab
                  String[] parts = text.toString().split("\t");
-                 row.put(Long.valueOf(parts[0]), parts[1]);
+                 line.put(Long.valueOf(parts[0]), parts[1]);
              }
-             String rowString = StringUtils.join(row.values(), ' ');
+             String rowString = StringUtils.join(line.values(), ' ');
+             //Ecrit dans le context, la clé de ligne et la ligne
              context.write(key, new Text(rowString));
       }
 		 }
 	
 	 public static void main(String[] args) throws Exception {
+		    //Configurations de l'environnement Hadoop
 		    Configuration conf = new Configuration();
 		    Job job = Job.getInstance(conf, "Pivot Hadoop");
 		    job.setJarByClass(MapReduce.class);
